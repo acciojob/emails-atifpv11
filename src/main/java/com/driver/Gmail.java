a7f35347da7c90a1d02aa6311f8215947850232a
atifpv11 <muhammedatif44@gmail.com>
2024-02-24 20:40:15 +0530

    changes added 3

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -91,6 +91,40 @@ public class Gmail extends Email {
 
     public int getInboxCapacity() {
         // Return the maximum number of mails that can be stored in the inbox
-        return inboxCapacity;
+        return this.inboxCapacity;
+    }
+    private class Mail {
+        private Date date;
+        private String sender;
+        private String message;
+
+        public Mail(Date date,String sender,String message){
+            this.date=date;
+            this.sender=sender;
+            this.message=message;
+        }
+        public Date getDate() {
+            return date;
+        }
+
+        public String getSender() {
+            return sender;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public void setSender(String sender) {
+            this.sender = sender;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
     }
 }
